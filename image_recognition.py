e93b220284f26e2de46a297903f809a16b827c1b
EmmettPangan <empangan@ucsd.edu>
2023-04-01 00:40:00 -0500

    Updated comments

--- image_recognition.py
+++ image_recognition.py
@@ -1,3 +1,9 @@
+# This file contains the code for training the image recognition model.
+# We used the Keras API to create a convolutional neural network that could
+# classify images into one of one hundred classes in the CIFAR-100 data set. 
+# The data set contains 60,000 color images, each one 32x32 pixels. 50,000 of these
+# images were used for training while 10,000 were saved for testing purposes.
+
 from tensorflow import keras
 from keras import Sequential
 from keras.layers import Dense, Activation, Flatten
@@ -17,7 +23,7 @@ labels = master_labels()
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data(label_mode = "fine")
 model = keras.models.load_model("image-recognition-model")
 
-# This code was initially used to create the model
+# This code was initially used to create and train the model, after which it was saved.
 # model = Sequential()
 # model.add(keras.Input(shape = x_train.shape[1:]))
 # model.add(Conv2D(8, (3, 3)))
@@ -36,29 +42,40 @@ model = keras.models.load_model("image-recognition-model")
 
 # model.evaluate(x_test, y_test, batch_size = 25)
 
-# Determines the model's accuracy on test images with some number of answer choices
+# Determines the model's accuracy on test images given some number of answer choices.
 def multiple_choice_model_accuracy(model, test_data, test_labels, num_choices = 4, repeats = 1000):
     num_correct = 0
-    total = 0
     for repeat in range(repeats):
+        # Randomly select one label from the list of test image labels.
+        # We will consider this label the correct answer and have the model
+        # make a prediction on its corresponding image.
         true_answer_index = random.randint(0, len(test_labels)-1)
         true_answer = test_labels[true_answer_index][0]
         answers = list()
         answers.append(true_answer)
+
+        # Randomly select other labels to serve as alternate,
+        # incorrect answer choices for the model.
         while len(answers) < num_choices:
             potential_answer = test_labels[random.randint(0, len(test_labels)-1)][0]
             if potential_answer not in answers:
                 answers.append(potential_answer)
+
+        # For the image corresponding to the correct answer,
+        # the model predicts the likelihood for each class to be its label.
         predictions = model.predict_on_batch(test_data[true_answer_index:true_answer_index+1])
+
+        # Determine the model's highest likelihood prediction out of the given answer choices.
         prediction_index = 0
         current_max = 0
         for i in answers:
             if predictions[0][i] > current_max:
                 current_max = predictions[0][i]
                 prediction_index = i
+        
+        # Check if the model's prediction was correct.
         if prediction_index == true_answer:
             num_correct += 1
-        total += 1
-    return num_correct/total
+    return num_correct / repeats
 
 print("Four Choice Accuracy =", multiple_choice_model_accuracy(model, x_test, y_test))
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-from lib2to3.pytree import convert
+# This file implements basic web app functionality using Flask,
+# creating a home page and two game mode pages.
+
 from flask import Flask
 from flask import render_template
-from flask import request
 from random import randint, shuffle
 from tensorflow import keras
 
 app = Flask(__name__)
 
+# These variables will be used to preload game data into each
+# game page so that they can be played asynchronously.
+# c contains every set of four answer choices to be displayed to the user.
+# a contains the correct answer choice for every image.
+# i contains the pixel data for every image to be displayed.
+# model_predictions contains the prediction of the model for every image.
 c = ""
 a = ""
 i = ""
@@ -16,11 +23,17 @@ model_predictions = ""
 def render_home():
     return render_template("home.html")
 
+# Generate data for 150 rounds, with each one having answer choices,
+# a correct answer, and the corresponding image to be displayed.
+# This data is passed to the page using Jinja2 templating.
 @app.route("/blitz")
 def render_blitz():
-    generate_data(100)
+    generate_data(150)
     return render_template("blitz.html", choices = c, answers = a, images = i)
 
+# Generate data for 150 rounds, with each one having answer choices,
+# a correct answer, and the corresponding image to be displayed.
+# This data is passed to the page using Jinja2 templating.
 @app.route("/mvm")
 def render_mvm():
     generate_data(150)
@@ -35,6 +48,7 @@ def unpickle(file, enc):
 def master_labels():
     return unpickle("CIFAR-100/meta", "ASCII")["fine_label_names"]
 
+# Unpack labels, training data, testing data, and the pre-trained model.
 labels = master_labels()
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar100.load_data(label_mode = "fine")
 model = keras.models.load_model("image-recognition-model")
@@ -42,11 +56,13 @@ model = keras.models.load_model("image-recognition-model")
 def generate_data(num_rounds):
     '''
     Returns the data for some number of rounds,
-    including the answer choices, correct answers, and image data for each round
+    including the answer choices, correct answers, and image data for each round.
     '''
     global c, a, i, model_predictions, x_test, y_test, labels
     c = a = i = model_predictions = ""
     for round in range(num_rounds):
+        # Concatenate the data for each round to the running strings c, a, i, and model_predictions,
+        # with rounds delimited by semicolons.
         if round == num_rounds-1:
             (x, y, z, s) = random_image(x_test, y_test, labels)
             c += x
@@ -66,12 +82,19 @@ def random_image(image_data, label_data, labels):
     returning the answer choices, correct answer, image data, and model prediction
     '''
     global model
-    converted_labs = ans = converted_im = m_score = ""
+    converted_labs = ans = converted_im = ""
 
+    # Randomly select an image and retrieve its image data.
     index = randint(0, len(image_data)-1)
     im = image_data[index].flatten()
+
+    # Determine the correct classification for the iamge.
     correct_lab_index = label_data[index][0]
     ans = labels[correct_lab_index]
+
+    # Generate a list for the index position of every selected label choice, and
+    # generate a list for the actual label choices themselves. These will include
+    # the correct label as well as alternate, incorrect answer choices.
     lab_indices = list()
     lab_indices.append(correct_lab_index)
     labs = list()
@@ -83,14 +106,19 @@ def random_image(image_data, label_data, labels):
             lab_indices.append(potential_index)
             labs.append(potential_label)
     
+    # Shuffle the answer choices.
     shuffle(labs)
 
+    # Convert the answer choices to a string, with choices delimited by commas.
     for i in range(len(labs)):
         if i == len(labs)-1:
             converted_labs += labs[i]
         else:
             converted_labs += labs[i] + ","
 
+    # Convert the image data to a string, with RGB values delimited by commas.
+    # Individual pixels are also delimited by commas and can be determined
+    # by from every set of three values, which correspond to RGB.
     for i in range(len(im)):
         if i == len(im)-1:
             converted_im += str(im[i]) + ",255"
@@ -99,13 +127,14 @@ def random_image(image_data, label_data, labels):
         else:
             converted_im += str(im[i]) + ","
         
+    # Make a prediction on the image using the model.
     prediction = predicted_label(model, x_test, index, lab_indices, labels)
     
     return (converted_labs, ans, converted_im, prediction)
 
 def predicted_label(model, image_data, correct_data_index, indices, labels):
     '''
-    Returns the predicted label for an image
+    Returns the predicted label for an image given the indices of the possible answer choices.
     '''
     predictions = model.predict_on_batch(image_data[correct_data_index:correct_data_index+1])
     max_index = 0
